04f94e896346e1be9e5d0ab1c60109e638aff516
fatpound <84704675+fatpound@users.noreply.github.com>
2024-11-18 20:54:36 +0300

    small refactoring on math cxx

--- Math/Math.cxx
+++ Math/Math.cxx
@@ -10,21 +10,20 @@ namespace fatpound::math
 {
     auto GetDifferenceVector(const ::dx::XMVECTOR pos1_vec, const ::dx::XMVECTOR pos2_vec) noexcept -> ::dx::XMVECTOR
     {
-        return dx::XMVectorSubtract(pos1_vec, pos2_vec);
+        return ::dx::XMVectorSubtract(pos1_vec, pos2_vec);
     }
 
-    auto GetDistanceBetweenXMF2(const::DirectX::XMFLOAT2& pos1, const::DirectX::XMFLOAT2& pos2) noexcept -> float
+    auto GetDistanceBetweenXMF2(const ::dx::XMFLOAT2& pos1, const ::dx::XMFLOAT2& pos2) noexcept -> float
     {
-        const auto& pos1Vec = DirectX::XMLoadFloat2(&pos1);
-        const auto& pos2Vec = DirectX::XMLoadFloat2(&pos2);
+        const auto& pos1Vec = ::dx::XMLoadFloat2(&pos1);
+        const auto& pos2Vec = ::dx::XMLoadFloat2(&pos2);
 
-        const auto& differenceVec = DirectX::XMVectorSubtract(pos1Vec, pos2Vec);
+        const auto& differenceVec = ::dx::XMVectorSubtract(pos1Vec, pos2Vec);
 
-        return DirectX::XMVectorGetX(
-            DirectX::XMVector2Length(differenceVec)
+        return ::dx::XMVectorGetX(
+            ::dx::XMVector2Length(differenceVec)
         );
     }
-
     auto GetDistanceBetweenXMF3(const ::dx::XMFLOAT3& pos1, const ::dx::XMFLOAT3& pos2) noexcept -> float
     {
         const auto& pos1Vec = ::dx::XMLoadFloat3(&pos1);
