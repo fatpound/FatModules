14eab972d94211eabd019aa3f949457362edbfa1
fatpound <84704675+fatpound@users.noreply.github.com>
2025-03-06 22:17:19 +0300

    enabled file module

--- File/File.cxx
+++ File/File.cxx
@@ -4,28 +4,16 @@ namespace fatpound::file
 {
     auto GetNameAndExtension(const std::string& filename) -> std::pair<std::string, std::string>
     {
-        if (std::filesystem::is_regular_file(filename))
+        if (not std::filesystem::is_regular_file(filename))
         {
-            const auto it = std::find(filename.rbegin(), filename.rend(), '.');
-            std::int64_t dot_index{};
-
-            if (it != filename.rend())
-            {
-                dot_index = it - filename.rbegin() + 1;
-            }
-
-            std::string temp_revstr(filename.rbegin(), it);
-            
-            std::reverse(temp_revstr.begin(), temp_revstr.end());
-
-            return {
-                std::string(filename.cbegin(), filename.cend() - dot_index),
-                std::string(temp_revstr)
-            };
+            throw std::runtime_error("The file does not exist!");
         }
-        else
-        {
-            throw std::runtime_error("The given path is not pointing to a file!");
-        }
+
+        const std::filesystem::path filePath(filename);
+
+        return {
+            filePath.stem().string(),
+            filePath.extension().string()
+        };
     }
 }
