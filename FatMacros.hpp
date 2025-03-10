01ed64fc3014a661c09e4feb0b1f2ab747236fed
fatpound <84704675+fatpound@users.noreply.github.com>
2025-03-10 16:38:23 +0300

    fixed missing guard around FAT_CMATH_CONSTEXPR23 macro

--- FatMacros.hpp
+++ FatMacros.hpp
@@ -2,10 +2,12 @@
 
 #include <version>
 
+#ifndef FAT_CMATH_CONSTEXPR23
 #ifdef __cpp_lib_constexpr_cmath
 #define FAT_CMATH_CONSTEXPR23 constexpr
 #else
 #define FAT_CMATH_CONSTEXPR23
-#endif // DEBUG
+#endif // __cpp_lib_constexpr_cmath
+#endif // FAT_CMATH_CONSTEXPR23
 
 #define IS_FRAMEWORK false
