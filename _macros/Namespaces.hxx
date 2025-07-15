662529b9c60df5238686ad75f3d90fd1c3e83b2f
fatpound <84704675+fatpound@users.noreply.github.com>
2025-07-15 22:08:45 +0300

    refactored namespace macros for consistency

--- _macros/Namespaces.hxx
+++ _macros/Namespaces.hxx
@@ -7,10 +7,12 @@
 #define FATSPACE_ALGORITHM            NAMESPACE_FATPOUND::algorithm
 #define FATSPACE_AUTOMATA             NAMESPACE_FATPOUND::automata
 #define FATSPACE_BIT                  NAMESPACE_FATPOUND::bit
-#define FATSPACE_COLORS               NAMESPACE_FATPOUND::colors
 #define FATSPACE_CONCURRENCY          NAMESPACE_FATPOUND::concurrency
 #define FATSPACE_CRYPTOGRAPHY         NAMESPACE_FATPOUND::cryptography
 #define FATSPACE_DSA                  NAMESPACE_FATPOUND::dsa
+#define FATSPACE_FILE                 NAMESPACE_FATPOUND::file
+#define FATSPACE_GEOMETRY             NAMESPACE_FATPOUND::geometry
+#define FATSPACE_GRAPH                NAMESPACE_FATPOUND::graph
 #define FATSPACE_IO                   NAMESPACE_FATPOUND::io
 #define FATSPACE_MATH                 NAMESPACE_FATPOUND::math
 #define FATSPACE_MEMORY               NAMESPACE_FATPOUND::memory
@@ -19,9 +21,8 @@
 #define FATSPACE_UTILITY              NAMESPACE_FATPOUND::utility
 #define FATSPACE_WIN32                NAMESPACE_FATPOUND::win32
 
+#define FATSPACE_COLORS               NAMESPACE_FATPOUND::colors
 #define FATSPACE_NUMBERS              FATSPACE_MATH::numbers
-#define FATSPACE_GEOMETRY             FATSPACE_MATH::geometry
 #define FATSPACE_UTILITY_GFX          FATSPACE_UTILITY::gfx
-
 #define FATSPACE_D2D                  FATSPACE_WIN32::d2d
 #define FATSPACE_D3D11                FATSPACE_WIN32::d3d11
